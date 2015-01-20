b0ff99838b8e845c5b014b042a9bbf0895f6c397
Marek Gilbert <mcg@google.com>
2015-01-20 09:47:57 -0800

    Authenticate the backend with a shared secret

--- sim/app.js
+++ sim/app.js
@@ -3,14 +3,37 @@ var CommandProcessor = require('./lib/commands');
 var PromisedFirebase = require('./lib/fbpromise');
 
 var Firebase = require('firebase');
+var FirebaseTokenGenerator = require("firebase-token-generator");
+
+var fs = require('fs');
 
 function main() {
-  var root = new Firebase('https://burning-inferno-9409.firebaseio.com/');
-  var promised = new PromisedFirebase(root);
-  var main = new MainCtrl(promised);
-  var commands = new CommandProcessor(main);
+  fs.readFile('secret.json', function(err, secret) {
+    if (err) {
+      console.log('Failed to read secret file: ' + err);
+      process.exit(2);
+    }
 
-  commands.processArgv();
+    secret = JSON.parse(secret);
+
+    var tokenGenerator = new FirebaseTokenGenerator(secret.secret);
+    var token = tokenGenerator.createToken({uid: "system", system: true});
+
+    var root = new Firebase('https://burning-inferno-9409.firebaseio.com/');
+    root.authWithCustomToken(token, function(err, authData) {
+      if (err) {
+        console.log('Failed to authenticate to Firebase using a secret: ' + err);
+        process.exit(2);
+      }
+
+      var promised = new PromisedFirebase(root);
+      var main = new MainCtrl(promised);
+      var commands = new CommandProcessor(main);
+
+      commands.processArgv();
+    });
+
+  });
 }
 
 main();
