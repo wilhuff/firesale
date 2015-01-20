b189cedb16d6cf69dd931b552a4369f06871b05d
Marek Gilbert <mcg@google.com>
2015-01-20 11:36:02 -0800

    Don't hardcode the Firebase for this app.

--- sim/app.js
+++ sim/app.js
@@ -19,12 +19,14 @@ function main() {
     var tokenGenerator = new FirebaseTokenGenerator(secret.secret);
     var token = tokenGenerator.createToken({uid: "system", system: true});
 
-    var root = new Firebase('https://burning-inferno-9409.firebaseio.com/');
+    var root = new Firebase(secret.firebase);
     root.authWithCustomToken(token, function(err, authData) {
       if (err) {
         console.log('Failed to authenticate to Firebase using a secret: ' + err);
         process.exit(2);
       }
+
+      console.log('Started Firebase.')
 
       var promised = new PromisedFirebase(root);
       var main = new MainCtrl(promised);
